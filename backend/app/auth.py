154e3ede7a93941ac1378ff840b17b06d9aed9ad
nandeeshnan <nandeeshkv11@gmail.com>
2025-03-11 17:11:46 +0530

    added secret key

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -15,7 +15,7 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
+#
 router = APIRouter()
 
 class UserCreate(BaseModel):
