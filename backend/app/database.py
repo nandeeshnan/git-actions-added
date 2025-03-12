43c49d0120b9aebf6be0d9b7052fc4776f1cbf2b
nandeeshnan <nandeeshkv11@gmail.com>
2025-03-12 19:40:13 +0530

    database url are encoded

--- backend/app/database.py
+++ backend/app/database.py
@@ -4,8 +4,8 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 load_dotenv()
-# DATABASE_URL = os.getenv("DATABASE_URL")
-DATABASE_URL = "postgresql+asyncpg://nandeesh:nandeesh123@database-1.cxm2omoga4r6.us-east-1.rds.amazonaws.com:5432/recipefind"
+DATABASE_URL = os.getenv("DATABASE_URL")
+# DATABASE_URL = "postgresql+asyncpg://nandeesh:nandeesh123@database-1.cxm2omoga4r6.us-east-1.rds.amazonaws.com:5432/recipefind"
 engine = create_async_engine(DATABASE_URL, echo=True)
 async_session = sessionmaker(
     engine, expire_on_commit=False, class_=AsyncSession
