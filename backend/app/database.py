4d415603119bf3ecd3291045325978d85c55ab19
nandeeshnan <nandeeshkv11@gmail.com>
2025-03-17 15:14:06 +0530

    added undo for both frontend and backend and added the wromg image

--- backend/app/database.py
+++ backend/app/database.py
@@ -5,7 +5,6 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
-# DATABASE_URL = "postgresql+asyncpg://nandeesh:nandeesh123@database-1.cxm2omoga4r6.us-east-1.rds.amazonaws.com:5432/recipefind"
 engine = create_async_engine(DATABASE_URL, echo=True)
 async_session = sessionmaker(
     engine, expire_on_commit=False, class_=AsyncSession
