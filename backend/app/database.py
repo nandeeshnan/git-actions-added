64ea098997fc39f4c4f57a13419911c03e2e6be0
nandeeshnan <nandeeshkv11@gmail.com>
2025-03-18 18:39:53 +0530

    added correct for testing

--- backend/app/database.py
+++ backend/app/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 load_dotenv()
-DATABASE_URL = os.getenv("DATABASE_UR")
+DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_async_engine(DATABASE_URL, echo=True)
 async_session = sessionmaker(
     engine, expire_on_commit=False, class_=AsyncSession
