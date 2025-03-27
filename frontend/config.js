3528271b5ba813c00f62a8c07f2db472c37eb73f
nandeeshnan <nandeeshkv11@gmail.com>
2025-03-27 14:46:33 +0530

    added tls certificate for backend 8000

--- frontend/config.js
+++ frontend/config.js
@@ -1,1 +1,2 @@
-window.BACKEND_URL = 'https://a8ac66ccab4764a5b9d62288b7d19a6c-735843869.us-east-1.elb.amazonaws.com:8000';
+// window.BACKEND_URL = 'https://a8ac66ccab4764a5b9d62288b7d19a6c-735843869.us-east-1.elb.amazonaws.com:8000';
+window.BACKEND_URL = 'https://recipe.sigmoid.io/auth';
